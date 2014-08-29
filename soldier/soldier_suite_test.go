bfb0dea68131807168d28edf55de5b933d250d34
Onsi Fakhouri <onsi@pivotallabs.com>
2014-08-29 12:57:18 -0700

    optimize integration suite: only build assets on node 1

--- soldier/soldier_suite_test.go
+++ soldier/soldier_suite_test.go
@@ -5,9 +5,26 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 )
+
+var soldier string
 
 func TestDockerCircusSoldier(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Docker-Circus-Soldier Suite")
 }
+
+var _ = SynchronizedBeforeSuite(func() []byte {
+	soldierPath, err := gexec.Build("github.com/cloudfoundry-incubator/docker-circus/soldier")
+	Ω(err).ShouldNot(HaveOccurred())
+	return []byte(soldierPath)
+}, func(soldierPath []byte) {
+	soldier = string(soldierPath)
+})
+
+var _ = SynchronizedAfterSuite(func() {
+	//noop
+}, func() {
+	gexec.CleanupBuildArtifacts()
+})
