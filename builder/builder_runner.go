guanglinlv/docker_app_lifecycle

Support for private images
[#86969554]

Signed-off-by: Georgi Sabev <georgethebeatle@gmail.com>

builder/builder_runner.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
@@ -24,6 +25,9 @@ type Builder struct {
 	DockerDaemonExecutablePath string
 	DockerDaemonTimeout        time.Duration
 	CacheDockerImage           bool
+	DockerLoginServer          string
+	DockerRegistryAuthToken    string
+	DockerRegistryEmail        string
 }
 
 func (builder *Builder) Run(signals <-chan os.Signal, ready chan<- struct{}) error {
@@ -36,7 +40,7 @@ func (builder *Builder) Run(signals <-chan os.Signal, ready chan<- struct{}) err
 	close(ready)
 
 	select {
-	case err := <-builder.fetchMetadata():
+	case err := <-builder.build():
 		if err != nil {
 			return err
 		}
@@ -47,7 +51,7 @@ func (builder *Builder) Run(signals <-chan os.Signal, ready chan<- struct{}) err
 	return nil
 }
 
-func (builder *Builder) fetchMetadata() <-chan error {
+func (builder *Builder) build() <-chan error {
 	errorChan := make(chan error, 1)
 
 	go func() {
@@ -105,8 +109,13 @@ func (builder *Builder) fetchMetadata() <-chan error {
 func (builder *Builder) cacheDockerImage(dockerImage string) (string, error) {
 	fmt.Println("Caching docker image ...")
 
+	err := builder.WriteDockerConfig()
+	if err != nil {
+		return "", err
+	}
+
 	fmt.Printf("Pulling docker image %s ...\n", dockerImage)
-	err := builder.RunDockerCommand("pull", dockerImage)
+	err = builder.RunDockerCommand("pull", dockerImage)
 	if err != nil {
 		return "", err
 	}
@@ -150,6 +159,21 @@ func (builder *Builder) GenerateImageName() (string, error) {
 		return "", err
 	}
 	return fmt.Sprintf("%s/%s", getRegistryAddress(builder.DockerRegistryAddresses), uuid), nil
+}
+
+func (builder *Builder) WriteDockerConfig() error {
+	if len(builder.DockerLoginServer) > 0 && len(builder.DockerRegistryAuthToken) > 0 && len(builder.DockerRegistryEmail) > 0 {
+		fmt.Printf("Saving configuration ...")
+
+		cfgTemplate := `{"%s": {"auth": "%s", "email": "%s"} }`
+		config := []byte(fmt.Sprintf(cfgTemplate, builder.DockerLoginServer, builder.DockerRegistryAuthToken, builder.DockerRegistryEmail))
+		err := ioutil.WriteFile(os.Getenv("HOME")+"/.dockercfg", config, 0644)
+
+		fmt.Println("Configuration saved.")
+		return err
+	}
+
+	return nil
 }
 
 func waitForDocker(signals <-chan os.Signal, timeout time.Duration) error {

builder/builder_runner_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"errors"
+	"io/ioutil"
 	"os"
 	"strings"
 	"time"
@@ -149,4 +150,46 @@ var _ = Describe("Builder runner", func() {
 
 	})
 
+	Describe("docker config", func() {
+		var builder main.Builder
+		confFile := os.Getenv("HOME") + "/.dockercfg"
+
+		AfterEach(func() {
+			os.Remove(confFile)
+		})
+
+		Context("with correct data", func() {
+
+			BeforeEach(func() {
+				builder = main.Builder{
+					DockerLoginServer:       "server",
+					DockerRegistryEmail:     "email",
+					DockerRegistryAuthToken: "token",
+				}
+				err := builder.WriteDockerConfig()
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("writes the config", func() {
+				bytes, err := ioutil.ReadFile(confFile)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(string(bytes)).To(Equal(`{"server": {"auth": "token", "email": "email"} }`))
+			})
+		})
+
+		Context("with missing data", func() {
+			BeforeEach(func() {
+				builder = main.Builder{
+					DockerLoginServer:       "server",
+					DockerRegistryAuthToken: "token",
+				}
+			})
+
+			It("does not write a config", func() {
+				_, err := os.Stat(confFile)
+				Expect(os.IsNotExist(err)).To(BeTrue())
+			})
+		})
+
+	})
 })

builder/builder_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"regexp"
 	"time"
 
 	. "github.com/cloudfoundry-incubator/docker_app_lifecycle/Godeps/_workspace/src/github.com/onsi/ginkgo"
@@ -26,6 +27,9 @@ var _ = Describe("Building", func() {
 		dockerRegistryAddresses    string
 		dockerDaemonExecutablePath string
 		cacheDockerImage           bool
+		dockerLoginServer          string
+		dockerRegistryAuthToken    string
+		dockerRegistryEmail        string
 		outputMetadataDir          string
 		outputMetadataJSONFilename string
 		fakeDockerRegistry         *ghttp.Server
@@ -93,6 +97,9 @@ var _ = Describe("Building", func() {
 		dockerRegistryAddresses = ""
 		dockerDaemonExecutablePath = ""
 		cacheDockerImage = false
+		dockerLoginServer = ""
+		dockerRegistryAuthToken = ""
+		dockerRegistryEmail = ""
 
 		outputMetadataDir, err = ioutil.TempDir("", "building-result")
 		Expect(err).NotTo(HaveOccurred())
@@ -126,6 +133,15 @@ var _ = Describe("Building", func() {
 		}
 		if len(dockerDaemonExecutablePath) > 0 {
 			args = append(args, "-dockerDaemonExecutablePath", dockerDaemonExecutablePath)
+		}
+		if len(dockerLoginServer) > 0 {
+			args = append(args, "-dockerLoginServer", dockerLoginServer)
+		}
+		if len(dockerRegistryAuthToken) > 0 {
+			args = append(args, "-dockerRegistryAuthToken", dockerRegistryAuthToken)
+		}
+		if len(dockerRegistryEmail) > 0 {
+			args = append(args, "-dockerRegistryEmail", dockerRegistryEmail)
 		}
 
 		builderCmd = exec.Command(builderPath, args...)
@@ -295,5 +311,84 @@ var _ = Describe("Building", func() {
 				})
 			})
 		})
+
+		Context("with invalid docker registry credentials", func() {
+			BeforeEach(func() {
+				parts, err := url.Parse(fakeDockerRegistry.URL())
+				Expect(err).NotTo(HaveOccurred())
+				dockerRegistryAddresses = parts.Host + ",10.244.2.6:80"
+				dockerRef = fmt.Sprintf("%s/some-repo", parts.Host)
+				cacheDockerImage = true
+			})
+
+			Context("without auth token", func() {
+				BeforeEach(func() {
+					dockerLoginServer = "http://loginserver.com"
+					dockerRegistryEmail = "mail@example.com"
+				})
+
+				It("errors", func() {
+					session := setupBuilder()
+					Eventually(session.Err).Should(gbytes.Say("missing flags: dockerLoginServer, dockerRegistryAuthToken and dockerRegistryEmail required simultaneously"))
+					Eventually(session).Should(gexec.Exit(1))
+				})
+			})
+
+			Context("without email", func() {
+				BeforeEach(func() {
+					dockerLoginServer = "http://loginserver.com"
+					dockerRegistryAuthToken = "token"
+				})
+
+				It("errors", func() {
+					session := setupBuilder()
+					Eventually(session.Err).Should(gbytes.Say("missing flags: dockerLoginServer, dockerRegistryAuthToken and dockerRegistryEmail required simultaneously"))
+					Eventually(session).Should(gexec.Exit(1))
+				})
+			})
+
+			Context("without login server", func() {
+				BeforeEach(func() {
+					dockerRegistryAuthToken = "token"
+					dockerRegistryEmail = "mail@example.com"
+				})
+
+				It("errors", func() {
+					session := setupBuilder()
+					Eventually(session.Err).Should(gbytes.Say("missing flags: dockerLoginServer, dockerRegistryAuthToken and dockerRegistryEmail required simultaneously"))
+					Eventually(session).Should(gexec.Exit(1))
+				})
+			})
+
+			Context("with invalid email", func() {
+				BeforeEach(func() {
+					dockerRegistryAuthToken = "token"
+					dockerRegistryEmail = "invalid email"
+					dockerLoginServer = "http://loginserver.com"
+				})
+
+				It("errors", func() {
+					session := setupBuilder()
+					Eventually(session.Err).Should(gbytes.Say(regexp.QuoteMeta(fmt.Sprintf("invalid dockerRegistryEmail [%s]", dockerRegistryEmail))))
+					Eventually(session).Should(gexec.Exit(1))
+				})
+			})
+
+			Context("with invalid login server URL", func() {
+				BeforeEach(func() {
+					dockerRegistryAuthToken = "token"
+					dockerRegistryEmail = "mail@example.com"
+					dockerLoginServer = "://missingSchema.com"
+				})
+
+				It("errors", func() {
+					session := setupBuilder()
+					Eventually(session.Err).Should(gbytes.Say(regexp.QuoteMeta(fmt.Sprintf("invalid dockerLoginServer [%s]", dockerLoginServer))))
+					Eventually(session).Should(gexec.Exit(1))
+				})
+			})
+		})
+
 	})
+
 })

cloudfoundry/dockerapplifecycle

builder/builder_runner.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
@@ -24,6 +25,9 @@ type Builder struct {
 	DockerDaemonExecutablePath string
 	DockerDaemonTimeout        time.Duration
 	CacheDockerImage           bool
+	DockerLoginServer          string
+	DockerAuthToken            string
+	DockerEmail                string
 }
 
 func (builder *Builder) Run(signals <-chan os.Signal, ready chan<- struct{}) error {
@@ -36,7 +40,7 @@ func (builder *Builder) Run(signals <-chan os.Signal, ready chan<- struct{}) err
 	close(ready)
 
 	select {
-	case err := <-builder.fetchMetadata():
+	case err := <-builder.build():
 		if err != nil {
 			return err
 		}
@@ -47,7 +51,7 @@ func (builder *Builder) Run(signals <-chan os.Signal, ready chan<- struct{}) err
 	return nil
 }
 
-func (builder *Builder) fetchMetadata() <-chan error {
+func (builder *Builder) build() <-chan error {
 	errorChan := make(chan error, 1)
 
 	go func() {
@@ -105,8 +109,13 @@ func (builder *Builder) fetchMetadata() <-chan error {
 func (builder *Builder) cacheDockerImage(dockerImage string) (string, error) {
 	fmt.Println("Caching docker image ...")
 
+	err := builder.WriteDockerConfig()
+	if err != nil {
+		return "", err
+	}
+
 	fmt.Printf("Pulling docker image %s ...\n", dockerImage)
-	err := builder.RunDockerCommand("pull", dockerImage)
+	err = builder.RunDockerCommand("pull", dockerImage)
 	if err != nil {
 		return "", err
 	}
@@ -150,6 +159,21 @@ func (builder *Builder) GenerateImageName() (string, error) {
 		return "", err
 	}
 	return fmt.Sprintf("%s/%s", getRegistryAddress(builder.DockerRegistryAddresses), uuid), nil
+}
+
+func (builder *Builder) WriteDockerConfig() error {
+	if len(builder.DockerLoginServer) > 0 && len(builder.DockerAuthToken) > 0 && len(builder.DockerEmail) > 0 {
+		fmt.Printf("Saving configuration ...")
+
+		cfgTemplate := `{"%s": {"auth": "%s", "email": "%s"} }`
+		config := []byte(fmt.Sprintf(cfgTemplate, builder.DockerLoginServer, builder.DockerAuthToken, builder.DockerEmail))
+		err := ioutil.WriteFile(os.Getenv("HOME")+"/.dockercfg", config, 0644)
+
+		fmt.Println("Configuration saved.")
+		return err
+	}
+
+	return nil
 }
 
 func waitForDocker(signals <-chan os.Signal, timeout time.Duration) error {

builder/builder_runner_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"errors"
+	"io/ioutil"
 	"os"
 	"strings"
 	"time"
@@ -149,4 +150,46 @@ var _ = Describe("Builder runner", func() {
 
 	})
 
+	Describe("docker config", func() {
+		var builder main.Builder
+		confFile := os.Getenv("HOME") + "/.dockercfg"
+
+		AfterEach(func() {
+			os.Remove(confFile)
+		})
+
+		Context("with correct data", func() {
+
+			BeforeEach(func() {
+				builder = main.Builder{
+					DockerLoginServer: "server",
+					DockerEmail:       "email",
+					DockerAuthToken:   "token",
+				}
+				err := builder.WriteDockerConfig()
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("writes the config", func() {
+				bytes, err := ioutil.ReadFile(confFile)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(string(bytes)).To(Equal(`{"server": {"auth": "token", "email": "email"} }`))
+			})
+		})
+
+		Context("with missing data", func() {
+			BeforeEach(func() {
+				builder = main.Builder{
+					DockerLoginServer: "server",
+					DockerAuthToken:   "token",
+				}
+			})
+
+			It("does not write a config", func() {
+				_, err := os.Stat(confFile)
+				Expect(os.IsNotExist(err)).To(BeTrue())
+			})
+		})
+
+	})
 })

builder/builder_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"regexp"
 	"time"
 
 	. "github.com/cloudfoundry-incubator/docker_app_lifecycle/Godeps/_workspace/src/github.com/onsi/ginkgo"
@@ -26,6 +27,9 @@ var _ = Describe("Building", func() {
 		dockerRegistryAddresses    string
 		dockerDaemonExecutablePath string
 		cacheDockerImage           bool
+		dockerLoginServer          string
+		dockerAuthToken            string
+		dockerEmail                string
 		outputMetadataDir          string
 		outputMetadataJSONFilename string
 		fakeDockerRegistry         *ghttp.Server
@@ -93,6 +97,9 @@ var _ = Describe("Building", func() {
 		dockerRegistryAddresses = ""
 		dockerDaemonExecutablePath = ""
 		cacheDockerImage = false
+		dockerLoginServer = ""
+		dockerAuthToken = ""
+		dockerEmail = ""
 
 		outputMetadataDir, err = ioutil.TempDir("", "building-result")
 		Expect(err).NotTo(HaveOccurred())
@@ -126,6 +133,15 @@ var _ = Describe("Building", func() {
 		}
 		if len(dockerDaemonExecutablePath) > 0 {
 			args = append(args, "-dockerDaemonExecutablePath", dockerDaemonExecutablePath)
+		}
+		if len(dockerLoginServer) > 0 {
+			args = append(args, "-dockerLoginServer", dockerLoginServer)
+		}
+		if len(dockerAuthToken) > 0 {
+			args = append(args, "-dockerAuthToken", dockerAuthToken)
+		}
+		if len(dockerEmail) > 0 {
+			args = append(args, "-dockerEmail", dockerEmail)
 		}
 
 		builderCmd = exec.Command(builderPath, args...)
@@ -295,5 +311,84 @@ var _ = Describe("Building", func() {
 				})
 			})
 		})
+
+		Context("with invalid docker registry credentials", func() {
+			BeforeEach(func() {
+				parts, err := url.Parse(fakeDockerRegistry.URL())
+				Expect(err).NotTo(HaveOccurred())
+				dockerRegistryAddresses = parts.Host + ",10.244.2.6:80"
+				dockerRef = fmt.Sprintf("%s/some-repo", parts.Host)
+				cacheDockerImage = true
+			})
+
+			Context("without auth token", func() {
+				BeforeEach(func() {
+					dockerLoginServer = "http://loginserver.com"
+					dockerEmail = "mail@example.com"
+				})
+
+				It("errors", func() {
+					session := setupBuilder()
+					Eventually(session.Err).Should(gbytes.Say("missing flags: dockerLoginServer, dockerAuthToken and dockerEmail required simultaneously"))
+					Eventually(session).Should(gexec.Exit(1))
+				})
+			})
+
+			Context("without email", func() {
+				BeforeEach(func() {
+					dockerLoginServer = "http://loginserver.com"
+					dockerAuthToken = "token"
+				})
+
+				It("errors", func() {
+					session := setupBuilder()
+					Eventually(session.Err).Should(gbytes.Say("missing flags: dockerLoginServer, dockerAuthToken and dockerEmail required simultaneously"))
+					Eventually(session).Should(gexec.Exit(1))
+				})
+			})
+
+			Context("without login server", func() {
+				BeforeEach(func() {
+					dockerAuthToken = "token"
+					dockerEmail = "mail@example.com"
+				})
+
+				It("errors", func() {
+					session := setupBuilder()
+					Eventually(session.Err).Should(gbytes.Say("missing flags: dockerLoginServer, dockerAuthToken and dockerEmail required simultaneously"))
+					Eventually(session).Should(gexec.Exit(1))
+				})
+			})
+
+			Context("with invalid email", func() {
+				BeforeEach(func() {
+					dockerAuthToken = "token"
+					dockerEmail = "invalid email"
+					dockerLoginServer = "http://loginserver.com"
+				})
+
+				It("errors", func() {
+					session := setupBuilder()
+					Eventually(session.Err).Should(gbytes.Say(regexp.QuoteMeta(fmt.Sprintf("invalid dockerEmail [%s]", dockerEmail))))
+					Eventually(session).Should(gexec.Exit(1))
+				})
+			})
+
+			Context("with invalid login server URL", func() {
+				BeforeEach(func() {
+					dockerAuthToken = "token"
+					dockerEmail = "mail@example.com"
+					dockerLoginServer = "://missingSchema.com"
+				})
+
+				It("errors", func() {
+					session := setupBuilder()
+					Eventually(session.Err).Should(gbytes.Say(regexp.QuoteMeta(fmt.Sprintf("invalid dockerLoginServer [%s]", dockerLoginServer))))
+					Eventually(session).Should(gexec.Exit(1))
+				})
+			})
+		})
+
 	})
+
 })